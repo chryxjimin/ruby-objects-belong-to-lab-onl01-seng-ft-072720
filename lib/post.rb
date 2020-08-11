e805840d85b9b4706867910f5f3ccbbbbfb6ece2
Christie Yoon <cleex1999@gmail.com>
2020-08-11 03:10:03 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,8 @@
+class Post
+   attr_accessor :title, :author
+
+   def initialize
+     @title = title
+    author.title  << self
+   end
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+   attr_accessor :title, :artist
+
+   def initialize
+      artist << self
+      artist.name
+      binding.pry
+   end
+end
